4ca89c20586f9d319cbbfdb7939185e4b204f814
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-31 17:16:04 +0300

    VR_ViveFocusPlus_ThrowingBalls (25.11.19)

---
+++ Assets/Firsus Games/RandomColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+    public Material material;
+    private void Awake()
+    {
+        material = this.GetComponent<Renderer>().material;
+        var r = Random.Range(0, 255)/100;
+        var g = Random.Range(0, 255)/100;
+        var b = Random.Range(0, 255)/100;
+        material.color = new Color(r, g, b, 1);
+    }
+}
